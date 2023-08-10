20c429a616aba68a419708c12b03bbe900da93ae
Mario Fouad <mariofouad03@gmail.com>
2023-08-10 16:33:43 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
-#include <stdio>
+#include <stdio.h>
+
 /**
- * main - Entry pomt
+ * main - Entry point
  *
  * Description: Program prints with put function
- * 
+ *
  * Return: Always 0 (Success)
-*/
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
+

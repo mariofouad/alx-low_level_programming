a63a02de6b2972ea7745f44b5123b8bacc1e682e
Mario Fouad <mariofouad03@gmail.com>
2023-08-11 11:23:50 -0700

    l

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Description: print numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}

92f90a9bd2247f60b203f057a226bca12e3a2f6f
Mario Fouad <mariofouad03@gmail.com>
2023-08-11 10:55:32 -0700

    l

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}

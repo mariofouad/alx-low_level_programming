f53b6269c47d881f2016e1709ca161b7cb560f38
Mario Fouad <mariofouad03@gmail.com>
2023-08-10 17:16:14 -0700

    l

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unisted.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints
+ *
+ * Return: alwas true 0 (Successful)
+*/
+int main(void)
+{
+char arr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1.arr.59);
+return (1);
+}

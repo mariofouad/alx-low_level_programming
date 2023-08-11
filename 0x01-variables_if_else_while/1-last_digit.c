647c8a90fbbae5cd334a545c4b755e4b6fc1995c
Mario Fouad <mariofouad03@gmail.com>
2023-08-11 09:18:42 -0700

    l

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	}
 	else if (lastdig == 0)
 	{
-		printf("Last digit of %d is %d and is zero\n", n, lastdig);
+		printf("Last digit of %d is %d and is 0\n", n, lastdig);
 	}
 	else
 	{

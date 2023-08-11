7c697e8bcd90b074b86979222a8404a4a93a4516
Mario Fouad <mariofouad03@gmail.com>
2023-08-11 12:19:02 -0700

    l

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Describtion: Print
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	int j;
+
+	for (i; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

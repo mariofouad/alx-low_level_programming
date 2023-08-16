83051a6a687d08b3773d9e630de28fca4c977a55
Mario Fouad <mariofouad03@gmail.com>
2023-08-16 08:17:59 -0700

    l

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10*the alp
+ */
+void print_alphabet_x10(void)
+{
+	int times = 0;
+	char ch;
+
+	while (time < 5)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		times++;
+	}
+	_putchar('\n');
+}

82fe3838689e48d952f0eb728fa38715bd4236d3
Mario Fouad <mariofouad03@gmail.com>
2023-08-16 09:51:37 -0700

    l

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - prints the minutes
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int min;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}

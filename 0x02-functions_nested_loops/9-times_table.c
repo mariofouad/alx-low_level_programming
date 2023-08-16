4c2455a0ef167f1ba08945b27da17effccffd20c
Mario Fouad <mariofouad03@gmail.com>
2023-08-16 16:21:06 -0700

    l

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,7 +18,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 			}
-			if (res < 10)
+			if (res < 10 && j != 0)
 			{
 				_putchar(' ');
 			}
@@ -32,7 +32,6 @@ void times_table(void)
 				_putchar(res + '0');
 			}
 		}
-		if(i != 9)
 			_putchar('\n');
 	}
 }

02eb59f82372e55711d1d09117d725ff95bc84e3
Mario Fouad <mariofouad03@gmail.com>
2023-08-16 16:47:31 -0700

    l

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,20 @@ void print_to_98(int n)
 			_putchar((n / 10) + '0');
 			_putchar((n % 10) + '0');
 		}
+		if(n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
 		n++;
+	}
+	while (n > 98)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		_putchar(',');
+		_putchar(' ');
+		n--;
 	}
 	_putchar('\n');
 }

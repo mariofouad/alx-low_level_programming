ad464f39005a04481719f933708c0e1c39e79c99
Mario Fouad <mariofouad03@gmail.com>
2023-08-16 16:38:33 -0700

    l

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_to_98 - print natural numbers
+ * @n: start
+ */
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		if (n < 10 && n > -10)
+		{
+			_putchar(n + '0');
+		}
+		else
+		{
+			_puchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		n++;
+	}
+}

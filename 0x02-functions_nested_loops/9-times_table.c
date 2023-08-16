4ac7c583e976d06f2df0315572f85c3cf3a0420c
Mario Fouad <mariofouad03@gmail.com>
2023-08-16 16:14:15 -0700

    l

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,11 +13,15 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			res = i * j;
-			if (j != 0)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                        }
+			if (j != 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (res < 10)
+			{
+				_putchar(' ');
+			}	
 			if (res >= 10)
 			{
 				_putchar((res / 10) + '0');

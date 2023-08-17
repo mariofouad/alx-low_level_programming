f9bc0e0db7702e09af27b616dde83ead36048613
Mario Fouad <mariofouad03@gmail.com>
2023-08-17 14:18:23 -0700

    l

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,15 +6,18 @@
  */
 void print_square(int size)
 {
+	int i;
+	int j;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (int i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (int j = 0; j < size; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}

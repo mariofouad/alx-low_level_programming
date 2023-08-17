fee5768095e69d562fe469df5a6259d38f149b03
Mario Fouad <mariofouad03@gmail.com>
2023-08-16 17:23:34 -0700

    l

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,45 +5,35 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n >= 98)
 	{
-		while (n <= 98)
+		for (n; n >= 98; n--)
 		{
-			if (n < 10 && n >= 0)
-			{
-				_putchar(n + '0');
-			}
-			else if (n < 0)
-			{
-				_putchar(45);
-				_putchar(((-1) * n) + '0');
-			}
-			else
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-			}
+			_putchar((n / 10) + '0');
+			if (n >= 100)
+				_putchar((n / 10) % 10 + '0');
+			_putchar((n % 10) + '0');
 			if (n != 98)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			n++;
 		}
 	}
 	else
 	{
-		while (n >= 98)
+		for (n; n <= 98; n++)
 		{
-			if (n < 100)
+			if (n < 0)
 			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
+				_putchar(45);
+				_puctchar((-1) * n + '0');
 			}
+			if (n < 10 && n >= 0)
+				_putchar(n + '0');
 			else
 			{
 				_putchar((n / 10) + '0');
-				_putchar((n / 10) % 10 + '0');
 				_putchar((n % 10) + '0');
 			}
 			if (n != 98)
@@ -51,8 +41,7 @@ void print_to_98(int n)
 				_putchar(',');
 				_putchar(' ');
 			}
-			n--;
 		}
 	}
-		_putchar('\n');
+	_putchar('\n');
 }

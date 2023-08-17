b57ead90b5db3202c83b6874893ff1796a06d191
Mario Fouad <mariofouad03@gmail.com>
2023-08-17 13:36:51 -0700

    l

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_diagonal - prints diagonal
+ * @n: number
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = n; i > 0; i--)
+	{
+		_putchar(92);
+		_putchar('\n');
+	}
+}

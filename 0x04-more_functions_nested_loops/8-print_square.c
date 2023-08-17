3326846bcbe4efe645e3a4738991336fd16c5dd8
Mario Fouad <mariofouad03@gmail.com>
2023-08-17 14:03:02 -0700

    l

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - prints a square of a given size
+ * @size: the size of the square
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}	
+			_putchar('\n');
+		}
+	}
+}
+

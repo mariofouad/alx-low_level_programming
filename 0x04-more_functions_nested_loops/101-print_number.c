85b4c8dbe7fb6fd81c8ebcf20adc749e801dd350
Mario Fouad <mariofouad03@gmail.com>
2023-08-17 14:36:08 -0700

    l

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,11 +9,11 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n = (-1) * n;
 	}
 	if (n / 10)
 	{
 		print_number(n / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar((n % 10) + '0');
 }

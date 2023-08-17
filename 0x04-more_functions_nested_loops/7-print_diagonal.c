219ceee625880e999c0093d81a07e64ab56cc164
Mario Fouad <mariofouad03@gmail.com>
2023-08-17 13:44:39 -0700

    l

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,10 +6,17 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	for (i = n; i > 0; i--)
 	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
 		_putchar(92);
 		_putchar('\n');
 	}
+	if (n <= 0)
+		_putchar('\n');
 }

37bbe0e3038dcfa6b5e68cf352f7d251a7416f6e
Mario Fouad <mariofouad03@gmail.com>
2023-08-22 16:21:00 -0700

    l

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,19 +14,29 @@ int _atoi(char *s)
 	int num = 0;
 	int i = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == '+')
-			p++;
-		else if (s[i] == '-')
-			n++;
-		else if (s[i] >= '0' && s[i] <= '9')
-			num = num * 10 + (s[i] - '0');
-		i++;
-	}
-
+while (s[i] != '\0')
+{
+if (s[i] == '+')
+	p++;
+else if (s[i] == '-')
+	n++;
+else if (s[i] >= '0' && s[i] <= '9')
+{
+if (num > INT_MAX / 10 || (num == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
+{
 	if (n % 2 == 1)
-		num = (-1) * num;
+		return (INT_MIN);
+	else
+		return (INT_MAX);
+}
+num = num * 10 + (s[i] - '0');
 
-	return (num);
+}
+i++;
+}
+
+if (n % 2 == 1)
+num = (-1) * num;
+
+return (num);
 }

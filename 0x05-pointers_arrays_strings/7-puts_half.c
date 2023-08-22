e3c1bc28ccf2702b797c2fb7c9d48c9d78a33ada
Mario Fouad <mariofouad03@gmail.com>
2023-08-22 10:51:22 -0700

    l

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,8 +9,13 @@ void puts_half(char *str)
 	int len = strlen(str);
 	int length = len - 1;
 	int i;
+	int start;
 
-	for (i = (len / 2); i <= length; i++)
+	if ((len % 2) == 0)
+		start = (len / 2);
+	else
+		start = (len / 2) + 1;
+	for (i = start; i <= length; i++)
 	{
 		putchar(str[i]);
 	}

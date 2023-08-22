a786b9a3a93d1035aa1ac63aad620c6b652757b7
Mario Fouad <mariofouad03@gmail.com>
2023-08-22 08:32:36 -0700

    l

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - gets the length
+ * @s: passed pointer
+ *
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int len = strlen(*s);
+	return (len);
+}

91c2a3495e7221dab6ea1725a999b952fddc9be1
Mario Fouad <mariofouad03@gmail.com>
2023-08-25 05:17:04 -0700

    l

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - capitalize fsts
+ * @s: fst
+ *
+ * Return: pointer
+ */
+char *cap_string(char *s)
+{
+	int len = strlen(s);
+	int i;
+	bool newword = true;
+
+	for (i = 0; i < len; i++)
+	{
+		if (new_word && (s[i] >= 'a' && s[i] <= 'z'))
+		{
+			s[i] -= 32;
+			newword = false;
+		}
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ','
+				|| s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?'
+				|| s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{'
+				|| s[i] == '}')
+		{
+			newword = true;
+		}
+	}
+	return (s);
+}

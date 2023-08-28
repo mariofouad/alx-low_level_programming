614d49758af0e7d0f48ee593c6c7d1302dafc17d
Mario Fouad <mariofouad03@gmail.com>
2023-08-28 15:07:20 -0700

    l

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strstr - ocates a substring
+ * @haystack: 1st
+ * @needle: 2nd
+ *
+ * Return: pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}

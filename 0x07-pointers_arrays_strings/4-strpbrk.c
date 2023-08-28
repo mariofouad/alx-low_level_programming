3063ee347c67bc28d6caba32ecf317cd209f0879
Mario Fouad <mariofouad03@gmail.com>
2023-08-28 15:01:01 -0700

    l

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strpbrk - searches a string
+ * @s: 1st
+ * @accept: 2nd
+ *
+ * Return: pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}

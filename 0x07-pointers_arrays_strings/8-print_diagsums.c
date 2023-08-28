535bb6c9c970cd00364235d10155def7163956ca
Mario Fouad <mariofouad03@gmail.com>
2023-08-28 15:42:53 -0700

    l

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagsums - sum of the two diagonals
+ * @a: 1st
+ * @size: 2nd
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int j;
+	int sum = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+				sum += putchar(a[i][j]);
+		}
+	}
+	printf(sum);
+}

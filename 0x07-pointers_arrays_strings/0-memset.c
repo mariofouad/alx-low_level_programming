b9666014895e1ebc292361dbcf2c869056735253
Mario Fouad <mariofouad03@gmail.com>
2023-08-28 14:33:39 -0700

    l

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _memset - fills memory
+ * @s: 1st
+ * @b: 2nd
+ * @n: 3rd
+ *
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memset(s, b, n));
+}

2d746aac90455b5907a0187f37cf81ab6628e8e8
Mario Fouad <mariofouad03@gmail.com>
2023-09-04 10:13:16 -0700

    l

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - start
+ * @argc: num
+ * @argv: pointer
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}

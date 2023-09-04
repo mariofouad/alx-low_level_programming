07af678a1e4bf750e73e9b9b308c2c1449064582
Mario Fouad <mariofouad03@gmail.com>
2023-09-04 10:10:42 -0700

    l

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - start
+ * @argc: num
+ * @argv: pointer
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

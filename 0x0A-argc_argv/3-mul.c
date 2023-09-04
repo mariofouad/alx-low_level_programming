342995ed9821699b0f15ce1b9bf965959a07baa7
Mario Fouad <mariofouad03@gmail.com>
2023-09-04 10:45:49 -0700

    l

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - start
+ * @argc: num
+ * @argv: pointer
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int product;
+
+	if (argc == 3)
+	{
+		product = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n",product);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}

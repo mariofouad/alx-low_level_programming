f1c328e5e87cef0f3d7adebb7c1a5cb77d2bb5a5
Mario Fouad <mariofouad03@gmail.com>
2023-09-05 14:02:51 -0700

    l

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,15 +11,32 @@ char *argstostr(int ac, char **av)
 	char *str;
 	int i;
 	int j;
+	int len = 0;
+	int Tlen = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	str = malloc(sizeof(char) * ac);
-
 	for (i = 0; i < ac; i++)
-		str[i] += argv [i];
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+		Tlen += len + 1;
+		len = 0;
+	}
 
-	str[argc] = '\0';
+	str = malloc(sizeof(char) * Tlen);
+
+	if (str == NULL)
+		return (NULL);
+
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			str[len++] = av[i][j];
+		str[len++] = (i == ac - 1) ? '\0' : ' ';
+	}
+
 	return (str);
 }

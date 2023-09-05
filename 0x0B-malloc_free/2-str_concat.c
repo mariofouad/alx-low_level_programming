5bd22e94ab9abd90c74f857fd5de7c8a8e541bb0
Mario Fouad <mariofouad03@gmail.com>
2023-09-05 04:40:13 -0700

    l

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * str_concat - concatenates strings
+ * @s1: str
+ * @s2: str
+ *
+ * Return: Pointer
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *arr;
+	int len1;
+	int len2;
+	int i;
+	int j;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+
+	if (s1 == NULL)
+		len1 = 0;
+	else
+		len1 = strlen(s1);
+
+	if (s2 == NULL)
+		len2 = 0;
+	else
+		len2 = strlen(s2);
+
+	arr = malloc(sizeof(char) * (len1 + len2 + 1));
+	
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+	       arr[i] = s1[i];
+
+	for (j = 0; j < len2; j++)
+		arr[len1 + j] = s2[j];
+
+	arr[len1 + len2 - 1] = '\0';
+	return (arr);
+}

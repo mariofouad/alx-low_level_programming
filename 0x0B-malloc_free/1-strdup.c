45109dd095930f014834e985f0e4847bb2a4d8f3
Mario Fouad <mariofouad03@gmail.com>
2023-09-05 04:06:36 -0700

    l

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,11 +11,16 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	nstr = malloc(sizeof(char) * (strlen(str)));
+
+	nstr = malloc(sizeof(char) * (strlen(str) + 1));
+
 	if (nstr == NULL)
 		return (NULL);
-	for (i = 0; i < strlen(str); i++)
+
+	for (i = 0; i <= strlen(str); i++)
 		nstr[i] = str[i];
+
 	return (nstr);
+
 	free(nstr);
 }

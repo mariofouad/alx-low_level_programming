5bada954d62328f5177fc1d975dd5d0ec6545387
Mario Fouad <mariofouad03@gmail.com>
2023-09-07 07:14:42 -0700

    l

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ * _realloc - realloc
+ * @ptr: old ptr
+ * @old_size: old
+ * @new_size: new
+ *
+ * Return: pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *optr;
+	unsigned int i;
+
+	if (old_size == new_size)
+
+	if (!ptr)
+	{
+		ptr = malloc(new_size);
+	}
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size > old_size)
+	{
+		optr = ptr;
+		ptr = malloc(new_size);
+		if (!ptr)
+		{
+			free(optr);
+			return (NULL);
+		}
+		memcpy(ptr, optr, old_size);
+		free(optr);
+
+	}
+	if (new_size < old_size)
+	{
+		optr = ptr;
+		ptr = malloc(new_size);
+		if (!ptr)
+		{
+			free(optr);
+			return (NULL);
+		}
+		memcpy(ptr, optr, new_size);
+		free(optr);
+	}
+	return (ptr);
+}

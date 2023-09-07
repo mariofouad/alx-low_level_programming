cfe2cd51e23f43d60550384d6c75482f265e409c
Mario Fouad <mariofouad03@gmail.com>
2023-09-07 05:12:36 -0700

    l

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	ptr = malloc(nmemb * size);
-	if (ptr)
+	if (!ptr)
 		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
 		ptr[i] = 0;

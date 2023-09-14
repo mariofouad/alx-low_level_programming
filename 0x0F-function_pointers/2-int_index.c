270d36b3bf79e84f8d74a484489eed225ba84d78
Mario Fouad <mariofouad03@gmail.com>
2023-09-14 16:37:05 -0700

    l

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,16 +10,14 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	int num;
 
 	if (array && size && cmp)
 	{
 		for (i = 0; i < size; i++)
 		{
-			num = (*cmp)(array[i]);
+			if ((*cmp)(array[i]))
+				return (i);
 		}
 	}
-	if (num == 0 || size <= 0)
-		return (-1);
-	return (num);
+	return (-1);
 }

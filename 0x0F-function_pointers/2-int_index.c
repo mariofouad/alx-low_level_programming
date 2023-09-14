51b410afaf3f70831af9ebd21c55ceeadd4fc0f4
Mario Fouad <mariofouad03@gmail.com>
2023-09-14 16:22:08 -0700

    l

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * int_index - searchen for an integer
+ * @array: array passed
+ * @size: size of array
+ * @cmp: pointer to funtion
+ *
+ * Return: number found
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && size && cmp)
+		for (i = 0; i < size; i++)
+		{
+			(*cmp)(array[i]);
+		}
+}

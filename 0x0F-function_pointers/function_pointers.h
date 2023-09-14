450a6a505e2bd8cda2b4f11ee8bfa7e513954b2c
Mario Fouad <mariofouad03@gmail.com>
2023-09-14 15:56:07 -0700

    l

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef FUN_H
 #define FUN_H
-#include <string>
-#include <stdlib>
+#include <string.h>
+#include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));

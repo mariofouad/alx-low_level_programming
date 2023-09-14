76cb8c1fb0e166a05ad27332e80b8f2121edb7ac
Mario Fouad <mariofouad03@gmail.com>
2023-09-14 16:08:50 -0700

    l

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - print passed arr and function
+ * @array: arr passed with numbers
+ * @size: size of arr
+ * @action: pointer to funtion
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array && size && action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			(*action)(array[i]);
+		}
+	}
+}

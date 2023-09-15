ebf44e7ae4fa3841bc7df52ee09d41039689118c
Mario Fouad <mariofouad03@gmail.com>
2023-09-14 18:27:16 -0700

    l

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - pr
+ * @separator: m
+ * @n: m
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, int));
+		if (seprator != NULL && i != n - 1)
+			printf("%s", seprator);
+	}
+	printf("\n");
+	va_end(list);
+}

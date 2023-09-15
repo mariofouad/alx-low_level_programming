3db7d2b2abd05f9f9f1598c0433d8787bfcdf692
Mario Fouad <mariofouad03@gmail.com>
2023-09-14 17:31:50 -0700

    l

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,30 +11,30 @@
  */
 int main(int argc, char *argv[])
 {
-    int bytes, i;
-    unsigned char *main_ptr;
+int bytes, i;
+unsigned char *main_ptr;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return 1;
-    }
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
 
-    bytes = atoi(argv[1]);
+bytes = atoi(argv[1]);
 
-    if (bytes < 0)
-    {
-        printf("Error\n");
-        return 2;
-    }
+if (bytes < 0)
+{
+printf("Error\n");
+return (2);
+}
 
-    main_ptr = (unsigned char *)main;
+main_ptr = (unsigned char *)main;
 
-    for (i = 0; i < bytes - 1; i++)
-        printf("%02x ", main_ptr[i]);
+for (i = 0; i < bytes - 1; i++)
+printf("%02x ", main_ptr[i]);
 
-    printf("%02x\n", main_ptr[i]);
+printf("%02x\n", main_ptr[i]);
 
-    return 0;
+return (0);
 }
 

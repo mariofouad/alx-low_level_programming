90f3756de3343dffeb726762a7f53839dfc873c9
Mario Fouad <mariofouad03@gmail.com>
2023-09-14 18:07:05 -0700

    l

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,19 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - check the code
+ * @n: fst num
+ * Return: Always 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum;
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, int);
+	}
+	va_end(list);
+	return (sum);

974f7f8b8cf34ccd03b153776d9ebc2d046ff1cc
mariofouad <mariofouad03@gmail.com>
2023-09-29 00:30:59 -0700

    l

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (*b != '0' && *b != '1')
 			return (0);
 		num = num * 2 + (*b++ - '0');
 	}

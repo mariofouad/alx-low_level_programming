bb49b7ab50935e838890e62cf1646552bca2a8c8
mariofouad <mariofouad03@gmail.com>
2023-10-02 23:18:22 -0700

    l

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,6 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len]; len++)
 			;
 		nw = write(fd, text_content, strlen(text_content));
+	}
 	if (nw == -1)
 	{
 		close(fd);
